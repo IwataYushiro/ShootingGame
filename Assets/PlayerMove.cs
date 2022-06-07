326b0515aa7b5438ad42f7b112bb1618fb97418f
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-07 14:47:29 +0900

    プレイヤー当たり判定も拡張

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -23,6 +23,36 @@ public class PlayerMove : MonoBehaviour
 
         //スクリーン画面右上の位置を固定
         RightTop = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));
+ 
+    //オブジェクトの数だけループ処理
+    foreach(Transform child in gameObject.transform)
+        {
+            //子オブジェクトの中で一番右の位置にいたなら
+            if(child.localPosition.x>=Right_)
+            {
+                //子オブジェクトのローカルX座標を右端用の変数に代入する
+                Right_=child.transform.localPosition.x;
+            }
+            //子オブジェクトの中で一番左の位置にいたなら
+            if (child.localPosition.x <= Left_)
+            {
+                //子オブジェクトのローカルX座標を左端用の変数に代入する
+                Left_ = child.transform.localPosition.x;
+            }
+            //子オブジェクトの中で一番上の位置にいたなら
+            if (child.localPosition.z >= Top_)
+            {
+                //子オブジェクトのローカルZ座標を上端用の変数に代入する
+                Top_ = child.transform.localPosition.z;
+            }
+            //子オブジェクトの中で一番下の位置にいたなら
+            if (child.localPosition.z <= Bottom_)
+            {
+                //子オブジェクトのローカルZ座標を下端用の変数に代入する
+                Bottom_ = child.transform.localPosition.z;
+            }
+
+        }
     }
 
     // Update is called once per frame
@@ -58,9 +88,9 @@ public class PlayerMove : MonoBehaviour
             //下方向に0.03動く
             pos.z -= 0.03f;
         }
-        transform.position=new Vector3
-            (Mathf.Clamp(pos.x,LeftBottom.x+transform.localScale.x,RightTop.x-transform.localScale.x),
+        transform.position = new Vector3
+            (Mathf.Clamp(pos.x, LeftBottom.x + transform.localScale.x - Left_, RightTop.x - transform.localScale.x - Right_),
             pos.y,
-            Mathf.Clamp(pos.z, LeftBottom.z + transform.localScale.z, RightTop.z - transform.localScale.z));
+            Mathf.Clamp(pos.z, LeftBottom.z + transform.localScale.z - Bottom_, RightTop.z - transform.localScale.z - Top_));
     }
 }
