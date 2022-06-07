3010185d1ffa6ecaf432def53bf6c138bc980b52
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-07 14:55:38 +0900

    敵のスクリプト追加

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //“G‚Ì‘Ì—Í—p
+    private int enemyHp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //¶¬‚É‘Ì—Í‚ğw’è
+        enemyHp = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //‚à‚µ‘Ì—Í‚ª0‚É‚È‚Á‚½‚ç
+        if(enemyHp<=0)
+        {
+            //©•ª‚ÅÁ‚¦‚é
+            Destroy(this.gameObject);
+        }
+    }
+}
