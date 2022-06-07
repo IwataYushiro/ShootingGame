753462af922eaa1eaa4a4e758c53ba41c76112ba
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-07 14:40:10 +0900

    プレイヤーが画面内に収まるようにした

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //カメラから見た画面左下の座標を入れる変数
+    Vector3 LeftBottom;
+    //カメラから見た画面右上の座標を入れる変数
+    Vector3 RightTop;
+
+    //子オブジェクトのサイズを入れるために必要
+    private float Left_, Right_, Top_, Bottom_;
+
     // Start is called before the first frame update
     void Start()
     {
-       
+        //カメラとプレイヤーの距離を測る(表示画面の西隅を設定するために必要)
+       var distance=Vector3.Distance(Camera.main.transform.position, transform.position);
+
+        //スクリーン画面左下の位置を固定
+        LeftBottom = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+
+        //スクリーン画面右上の位置を固定
+        RightTop = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));
     }
 
     // Update is called once per frame
@@ -43,6 +58,9 @@ public class PlayerMove : MonoBehaviour
             //下方向に0.03動く
             pos.z -= 0.03f;
         }
-        transform.position=new Vector3(pos.x, pos.y, pos.z);
+        transform.position=new Vector3
+            (Mathf.Clamp(pos.x,LeftBottom.x+transform.localScale.x,RightTop.x-transform.localScale.x),
+            pos.y,
+            Mathf.Clamp(pos.z, LeftBottom.z + transform.localScale.z, RightTop.z - transform.localScale.z));
     }
 }
