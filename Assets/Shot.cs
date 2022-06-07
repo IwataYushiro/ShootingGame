338d25ab1bc72941cb810cdfe7f3c457e4c74c16
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-07 13:45:53 +0900

    これにて弾が発射されました

--- Assets/Shot.cs
+++ Assets/Shot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Shot : MonoBehaviour
 {
+    //ゲームオブジェクトをインスペクターから参照するための変数
+    public GameObject Bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Shot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //弾を生成する
+            Instantiate(Bullet, transform.position, Quaternion.identity);
+        }
     }
 }
