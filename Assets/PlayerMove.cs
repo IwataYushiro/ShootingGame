9e9e48f5d7458d7a272b4a475bc3fe1c5863f95d
Yuushiro_Iwata <k021g1138@g.neec.ac.jp>
2022-06-07 13:32:43 +0900

    プレイヤーの移動追加

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -7,12 +7,42 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         //プレイヤーのワールド座標を取得
+        Vector3 pos=transform.position;
+
+        //右矢印キーが入力されたとき
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            //右方向に0.03動く
+            pos.x += 0.03f;
+        }
+
+        //左矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //左方向に0.03動く
+            pos.x -= 0.03f;
+        }
+
+        //上矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //上方向に0.03動く
+            pos.z += 0.03f;
+        }
+
+        //下矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //下方向に0.03動く
+            pos.z -= 0.03f;
+        }
+        transform.position=new Vector3(pos.x, pos.y, pos.z);
     }
 }
